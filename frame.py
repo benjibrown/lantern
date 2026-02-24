99613c5a39b003ef708c19e5a12f13f9779e1106
benji <benjnib09@gmail.com>
2026-02-24 21:11:28 +0000

    udp who? starting swap to tcp

---
+++ frame.py
@@ -0,0 +1,31 @@
+# yes, im restructuring lantern so it uses TCP not UDP 
+# i fear that udp has made be lose more brain cells than packets it has lost# which is a lot
+
+import socket 
+
+
+MAX_MESSAGE_BYTES = 10 * 1024 * 1024 # 10mb should be way more than enough
+
+
+def _recv_exact(sock: socket.socket, n: int):
+    # read bytes and return none if disconnected 
+    buf = b""
+    
+    while len(buf) < n:
+        try:
+            # chunky
+            chunk = sock.recv(n - len(buf))
+        except OSError:
+            return None
+        if not chunk:
+            return None
+        buf += chunk
+    return buf
+
+def send_msg(sock: socket.socket, text: str):
+    # send msg - length prefixed 
+    # TODO - encrypt here 
+    data = text.encode()
+    sock.sendall(len(data).to_bytes(4, "big") + data)
+
+

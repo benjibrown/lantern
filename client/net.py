cf8b2b9ab773401e038a4b604f756aa0d569f050
benji <benjnib09@gmail.com>
2026-02-26 13:30:27 +0000

    temp remove notis, notify-send throws error

--- client/net.py
+++ client/net.py
@@ -418,7 +418,8 @@ class NetworkManager:
                         -self.config.MAX_MESSAGES :
                     ]
                     # TODO - ANSII detection for focus - only show noti if window not focused, this is fine for now, if u hate notis run /dnd or ctrl+d
-                    if not is_self and not self.state.dnd:
+                   '''
+                   if not is_self and not self.state.dnd:
                         try:
                             if platform.system() == "Darwin":
                                 subprocess.run(
@@ -432,6 +433,7 @@ class NetworkManager:
                                 subprocess.run(["notify-send", "New Message", msg[:50]])
                         except Exception:
                             pass
+                    '''
 
             except BlockingIOError:
                 time.sleep(0.03)
